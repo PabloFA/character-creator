6eb801152dc15de4ddd96b848ae836a31218d6cf
Pablo Avendaño <avendanio.pablo@gmail.com>
2024-01-18 17:12:04 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <CharacterEditor />
+      <Footer />
+    </>
+  )
+}
+
+export default App
